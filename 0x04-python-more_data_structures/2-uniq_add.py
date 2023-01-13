1f44d601516b1c6e2fde047fefe7d53dbfcf7def
Biiddo <abiodunadejumo77@gmail.com>
2023-01-13 23:04:03 +0000

    task0

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
-q_add(my_list=[]):
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
     """Add all unique integers in a list (once for each integer)."""
     result = 0
     for x in set(my_list):
